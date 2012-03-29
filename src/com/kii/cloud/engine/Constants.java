5c23cd6c52cf54091ca7673f0b8d3f9a3b747a7c
yang.tian <yang.tian@e7cb0379-d053-0e4b-a1ac-43c7dbffadbe>
2012-03-29 06:59:32 +0000

    TianYang: use new appKey and appId
    git-svn-id: https://source.kii.com/servo/platform/sdk-mobile-client/androidV1/Samples/KiiFileStorageSampleApp@21894 e7cb0379-d053-0e4b-a1ac-43c7dbffadbe

--- src/com/kii/cloud/engine/Constants.java
+++ src/com/kii/cloud/engine/Constants.java
@@ -1,8 +1,8 @@
 package com.kii.cloud.engine;
 
 public class Constants {
-    public static final String appId = "6b587cee";
-    public static final String appKey = "82946bf6961f4e34d3a53bbc941ac115";
+    public static final String appId = "20df2cb0";
+    public static final String appKey = "18cb6a798b6367844ee96e55f3b71f86";
     public static final String DEFAULT_BASE_URL =  "http://dev-usergrid.kii.com:12110";
     public static final String UI_REFRESH_INTENT = "com.kii.cloud.demo.kiicloudstorage2.uirefresh";
     public static final String ANDROID_EXT = "ANDROID_EXT";
